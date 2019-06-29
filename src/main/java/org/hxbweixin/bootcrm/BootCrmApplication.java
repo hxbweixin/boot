4aaa77dc287b29b1f45af4641a608a04ebe81ddc
Administrator <Administrator@PC-201903051512>
2019-06-29 14:51:28 +0800

    初次提交

---
+++ src/main/java/org/hxbweixin/bootcrm/BootCrmApplication.java
@@ -0,0 +1,13 @@
+package org.hxbweixin.bootcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCrmApplication.class, args);
+	}
+
+}
